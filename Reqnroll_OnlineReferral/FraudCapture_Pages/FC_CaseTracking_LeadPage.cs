471c9a0cda3499c5574887e0819f06424053bec6
Sandeep Krishnan <sandeep.krishnan@gainwelltechnologies.com>
2026-01-23 08:58:03 +0530

    Adding pages to Fraud Capture module.

---
+++ Reqnroll_OnlineReferral/FraudCapture_Pages/FC_CaseTracking_LeadPage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FC_OnlineReferral.FraudCapture_Pages
+{
+    internal class FC_CaseTracking_LeadPage: BaseSettings
+    {
+        public FC_CaseTracking_LeadPage(IWebDriver driver) : base(driver) { }
+
+        #region Elements
+        //Add xpath here
+        private readonly By LeadID = By.XPath("//input[@id='']");
+        
+
+
+        #endregion
+    }
+}
