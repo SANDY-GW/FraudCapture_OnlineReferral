9fbeec0c67e3d682fd56ab4ed4fca8d9bbadbdc4
Sandeep Krishnan <sandeep.krishnan@gainwelltechnologies.com>
2026-03-19 14:28:10 +0530

    added Element Actions class

---
+++ Reqnroll_OnlineReferral/ElementActions.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FC_OnlineReferral
+{
+    internal class ElementActions
+    {
+
+        private readonly IWebDriver _driver;
+        private readonly WebDriverWait _wait;
+        //private readonly ScenarioContext _scenarioContext;
+
+        public ElementActions(IWebDriver driver)
+        {
+            _driver = driver;
+            _wait = new WebDriverWait(new SystemClock(), _driver, TimeSpan.FromSeconds(10), TimeSpan.FromMilliseconds(500));
+        }
+
+
+        public IWebElement WaitForVisible(By locator, int timeoutSeconds = 10)
+        {
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(timeoutSeconds));
+            return wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(locator));
+        }
+
+
+        public void EnterText(By locator, string text, bool clearFirst = true)
+        {
+            var element = WaitForVisible(locator);
+            if (clearFirst)
+                element.Clear();
+            element.SendKeys(text);
+        }
+
+        public string GetText(By locator)
+        {
+            return WaitForVisible(locator).Text;
+        }
+    }
+}
